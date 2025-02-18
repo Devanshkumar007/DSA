c90a2a419b3149ea34295265593ed45045b72594
Devansh Kumar <118431081+Devanshkumar007@users.noreply.github.com>
2025-02-18 22:47:56 +0530

    Time: 1 ms (100.00%) | Memory: 49.7 MB (52.46%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,43 +1,40 @@
-class Solution {
-    public void search(int i, int j, char[][] grid, boolean[][] visited) {
-        int m = grid.length, n = grid[0].length;
-        Queue<Pair<Integer, Integer>> queue = new LinkedList<>();
-        queue.offer(new Pair<>(i, j));
-        visited[i][j] = true;
+public class Solution {
+    private int rows; 
+    private int cols; 
+    private int island = 0; 
 
-        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    public int numIslands(char[][] grid) {
+        rows = grid.length;
+        cols = grid[0].length;
 
-        while (!queue.isEmpty()) {
-            Pair<Integer, Integer> ij = queue.poll();
-            int curri = ij.getKey();
-            int currj = ij.getValue();
+        for (int row = 0; row < rows; row++) {
+            search(grid, row); 
+        }
 
-            for (int[] dir : directions) {
-                int ni = curri + dir[0];
-                int nj = currj + dir[1];
+        return island; 
+    }
 
-                if (ni >= 0 && ni < m && nj >= 0 && nj < n && grid[ni][nj] == '1' && !visited[ni][nj]) {
-                    queue.offer(new Pair<>(ni, nj));
-                    visited[ni][nj] = true; 
-                }
+    public void search(char[][] grid, int row) {
+        char[] finalrow = grid[row];
+
+        for (int col = 0; col < cols; ++col) {
+            if (finalrow[col] == '1') { 
+                mark(grid, row, col); 
+                island++; 
             }
         }
     }
 
-    public int numIslands(char[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        boolean[][] visited = new boolean[m][n];
-        int count = 0;
+    public void mark(char[][] grid, int row, int col) {
+        grid[row][col] = '*'; 
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (!visited[i][j] && grid[i][j] == '1') {
-                    count++;
-                    search(i, j, grid, visited);
-                }
-            }
-        }
-        return count;
+        if (row > 0 && grid[row - 1][col] == '1')
+            mark(grid, row - 1, col);
+        if (row + 1 < rows && grid[row + 1][col] == '1')
+            mark(grid, row + 1, col);
+        if (col > 0 && grid[row][col - 1] == '1')
+            mark(grid, row, col - 1);
+        if (col + 1 < cols && grid[row][col + 1] == '1')
+            mark(grid, row, col + 1);
     }
 }
