07c24e14fea05b5f7a90308a45e803c96950d796
Devansh Kumar <118431081+Devanshkumar007@users.noreply.github.com>
2025-02-19 16:45:03 +0530

    Time: 2 ms (70.90%) | Memory: 42.4 MB (74.51%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,50 +1,51 @@
+class Pair{
+    int i;
+    int j;
+    int t;
+    Pair(int i ,int j,int t){
+        this.i = i ; this.j = j ; this.t=t; 
+    }
+}
+
 class Solution {
     public int orangesRotting(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
         boolean visited[][] = new boolean[m][n];
-        int min = 0 ;
-        boolean executed = true ;
-        while(executed){
-        Queue<Pair<Integer,Integer>> queue = new LinkedList<>();
+        Queue<Pair> queue = new LinkedList<>();
+        
+        int countFresh = 0 ;
+        for(int i=0 ; i<m ; i++){
+            for(int j=0 ; j<n ; j++){
+                if(grid[i][j]==2){
+                    visited[i][j] = true;
+                    queue.offer(new Pair(i,j,0));
+                }else if(grid[i][j]==1) countFresh++;
+            }
+        }
+
+        int time = 0;
+        int[][] dir = { {-1, 0}, {0, 1}, {1, 0}, {0, -1} };
         int count = 0 ;
-        //put true for curr min
-        for(int i= 0 ;i<m ; i++){
-            for(int j=0 ; j<n ; j++){
-                if(grid[i][j]==2 && !visited[i][j] ){
-                    count++;
-                    visited[i][j]= true ;
-                    queue.add(new Pair<>(i,j));
-                }
-            }
-        }
-        
-        // rotten for curr min 
-        if(!queue.isEmpty()) min++;
         while(!queue.isEmpty()){
-            Pair<Integer, Integer> ij = queue.poll();
-            int i = ij.getKey();
-            int j = ij.getValue();
-            //System.out.println(ij);
-
-               try{if(grid[i-1][j]==1) grid[i-1][j] = 2;} catch(Exception e) {}
-               try{if(grid[i+1][j]==1) grid[i+1][j] = 2;} catch (Exception e) {}
-               try{if(grid[i][j+1]==1) grid[i][j+1] = 2;} catch (Exception e) {}
-               try{if(grid[i][j-1]==1) grid[i][j-1] = 2;} catch (Exception e) {}
-            
-        }
-        
-        if(count==0) executed = false; 
-
-    }
-
-    for(int i= 0 ;i<m ; i++){
-            for(int j=0 ; j<n ; j++){
-                if(grid[i][j]==1){
-                    return -1;
+            int r = queue.peek().i;
+            int c = queue.peek().j;
+            int t = queue.peek().t;
+            time = Math.max(time,t);
+            queue.poll();
+            for(int i=0 ; i<4 ; i++){
+                int row = r + dir[i][0];
+                int col = c + dir[i][1];
+                if(row>=0 && row<m && col>=0 && col<n && !visited[row][col] && 
+                grid[row][col]==1){
+                    queue.offer(new Pair(row,col,t+1));
+                    visited[row][col]=true;
+                    count++;
                 }
             }
-    }
-    return (min!=0) ? min-1 : 0 ;
+        }
+        if(count != countFresh) return -1 ;
+        return time;
+
     }
 }
