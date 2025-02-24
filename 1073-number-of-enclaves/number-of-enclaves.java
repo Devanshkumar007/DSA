a912cb45f13900d8984505d18525e7d19af577d5
Devansh Kumar <118431081+Devanshkumar007@users.noreply.github.com>
2025-02-24 20:10:22 +0530

    Time: 8 ms (75.52%) | Memory: 53.5 MB (88.29%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -5,17 +5,13 @@ class Pair{
         this.i=i; this.j=j;
     }
 }
-
 class Solution {
-    int count = 0 ;
     public void bfs(int i, int j, boolean[][] visited , int[][] grid){
         visited[i][j]= true;
         Queue<Pair> q = new LinkedList<>();
         q.add(new Pair(i,j));
 
         int[][] dir= { {1,0} , {-1,0} , {0,1} , {0,-1} };
-        int curr = 1;
-        boolean possible = false ;
         
         while(!q.isEmpty()){
             int r =  q.peek().i;
@@ -24,28 +20,30 @@ class Solution {
             for(int k=0 ; k<4 ; k++){
                 int row = r + dir[k][0];
                 int col = c + dir[k][1];
-                if(row>=0 && row<grid.length && col >=0 && col<grid[0].length ){
-                    if(!visited[row][col] && grid[row][col]==1){
-                        curr++;
+                if( row>=0 && row<grid.length && col >=0 && col<grid[0].length && 
+                    !visited[row][col] && grid[row][col]==1 ){ 
                         visited[row][col]=true;
                         q.add(new Pair(row,col));
-                    }
-                }else{
-                    possible = true;
                 }
             }   
         }
-        if(!possible) count+=curr;
+
     }
     public int numEnclaves(int[][] grid) {
         int m = grid.length; int n = grid[0].length;
         boolean[][] visited = new boolean[m][n];
         for(int i=0 ; i<m ; i++){
-            for(int j=0 ; j<n ; j++){
-                if(!visited[i][j] && grid[i][j]==1){
-                    //System.out.println(i+" "+j);
-                    bfs(i,j,visited,grid);
-                }
+            if(grid[i][0]==1 && !visited[i][0]) bfs(i,0,visited,grid);
+            if(grid[i][n-1]==1 && !visited[i][n-1]) bfs(i,n-1,visited,grid);
+        }
+        for(int i=0 ; i<n; i++){
+            if(grid[0][i]==1 && !visited[0][i]) bfs(0,i,visited,grid);
+            if(grid[m-1][i]==1 && !visited[m-1][i]) bfs(m-1,i,visited,grid);
+        }
+        int count = 0 ;
+        for(int i=1 ; i<m-1 ; i++){
+            for(int j=1 ; j<n-1; j++){
+                if(!visited[i][j] && grid[i][j]==1) count++;
             }
         }
         return count;
