2e4256d3f8ed7ed803144ad70939ea81ef5059c1
Devansh Kumar <118431081+Devanshkumar007@users.noreply.github.com>
2025-03-25 22:43:16 +0530

    Time: 6 ms (25.02%) | Memory: 52.3 MB (19.78%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,45 +1,45 @@
 class Pair{
-    int i;
-    int j;
-    Pair(int i , int j){
-        this.i=i; this.j=j;
+    int r;
+    int c;
+    Pair(int r,int c){
+        this.r = r ; this.c = c;
     }
 }
+
 class Solution {
+    public void bfs (int i , int j ,boolean[][] visited , char[][] grid){
+        visited[i][j]= true ;
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(i,j));
+        int[][] dir = { {0,1},{1,0},{0,-1},{-1,0} };
+        while(!q.isEmpty()){
+            int r = q.peek().r ;
+            int c = q.peek().c ;
+            q.poll();
+            for(int k=0 ; k<4 ; k++){
+                int row = r+dir[k][0];
+                int col = c+dir[k][1];
+                if(row>=0 && row<grid.length && col>=0 && col<grid[0].length && 
+                grid[row][col]=='1' && !visited[row][col]){
+                    q.add(new Pair(row,col));
+                    visited[row][col]=true;
+                }
+            }
+            
+        }
+    }
+    
     public int numIslands(char[][] grid) {
-        int m = grid.length ; int n= grid[0].length;
-        boolean[][] visited = new boolean[m][n];
-        int islands = 0 ;
-        for(int i=0;i<m ; i++){
-            for(int j=0 ; j<n ; j++){
-                if(!visited[i][j] && grid[i][j]=='1'){
-                    islands++;
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        int islands= 0 ;
+        for(int i=0 ; i<grid.length ; i++){
+            for(int j=0 ; j<grid[0].length ; j++){
+                if(!visited[i][j] && grid[i][j]=='1') {
                     bfs(i,j,visited,grid);
+                    islands++;
                 }
             }
         }
         return islands;
-    }
-    public void bfs(int i, int j , boolean[][] visited , char[][]grid){
-        
-        visited[i][j]=true;
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(i,j));
-
-        int[][] dir = {{0,1},{0,-1},{-1,0},{1,0}};
-        while(!q.isEmpty()){
-            int r = q.peek().i;
-            int c = q.peek().j;
-            q.remove();
-            for(int k=0 ; k<4 ; k++){
-                int row = r + dir[k][0];
-                int col = c + dir[k][1];
-                if(row>=0 && row<grid.length && col>=0 && col<grid[0].length &&
-                !visited[row][col] && grid[row][col]=='1'){
-                    visited[row][col]=true;
-                    q.add(new Pair(row,col));
-                }
-            }
-        }
     }
 }
