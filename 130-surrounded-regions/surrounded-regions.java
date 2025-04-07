dc28685035d9a88e46420a45dbfbd0946de09ec3
Devansh Kumar <118431081+Devanshkumar007@users.noreply.github.com>
2025-04-07 17:21:24 +0530

    Time: 3 ms (33.63%) | Memory: 45.7 MB (29.48%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,61 +1,60 @@
 class Pair{
     int i;
     int j;
-    Pair(int i , int j){
-        this.i = i ; this.j = j;
-    } 
+    Pair(int i,int j){
+        this.i=i; this.j=j;
+    }
 }
 class Solution {
-    public void bfs(int i, int j , boolean[][] visited , char[][] board){
-        Queue<Pair> q = new LinkedList<>();
-        Queue<Pair> q1 = new LinkedList<>();
-
-        q.add(new Pair(i,j));
+    public void region(char[][] board , boolean[][] visited, int i, int j){
         visited[i][j]=true;
         int[][] dir={{0,1},{0,-1},{1,0},{-1,0}};
-        boolean possible = false ;
+        Queue<Pair> q = new LinkedList<>();
+        int m = board.length;
+        int n = board[0].length;
+        q.add(new Pair(i,j));
 
         while(!q.isEmpty()){
-            int r = q.peek().i;
-            int c = q.peek().j;
+            int row = q.peek().i;
+            int col = q.peek().j;
             q.remove();
-            q1.add(new Pair(r,c));
-            for(int k=0 ; k<4 ; k++){
-                int row = r + dir[k][0];
-                int col = c + dir[k][1];
-                if(row>=0 && row<board.length && col>=0 && col<board[0].length){
-                    if(board[row][col]=='O' && !visited[row][col]){
-                        visited[row][col]=true;
-                        q.add(new Pair(row,col));
-                    }
-                }else{
-                    possible =true;
+            for(int k=0;k<4;k++){
+                int r=row+dir[k][0];
+                int c=col+dir[k][1];
+                if(r>=0 && r<m && c>=0 && c<n && board[r][c]=='O' && !visited[r][c]){
+                    visited[r][c]=true;
+                    q.add(new Pair(r,c));
                 }
             }
-
         }
-        if(!possible){
-            while(!q1.isEmpty()){
-                int x = q1.peek().i;
-                int y = q1.peek().j;
-                board[x][y]='X';
-                q1.remove();
-                }
-            }
     }
 
-
     public void solve(char[][] board) {
-        int m = board.length ; 
+        int m = board.length;
         int n = board[0].length;
         boolean[][] visited = new boolean[m][n];
-        for(int i=0 ; i<m ;i++){
-            for(int j=0 ; j<n ; j++){
-                    if(board[i][j]=='O' && !visited[i][j]){
-                        bfs(i,j,visited,board);
-                    }
-                }
+        for(int i=0 ; i<n ;i++){
+            if(board[0][i]=='O' && !visited[0][i]){
+                region(board,visited,0,i);
+            }
+            if(board[m-1][i]=='O' && !visited[m-1][i]){
+                region(board,visited,m-1,i);
+            }
+        }
+        for(int i=1 ; i<m-1 ;i++){
+            if(board[i][0]=='O' && !visited[i][0]){
+                region(board,visited,i,0);
+            }
+            if(board[i][n-1]=='O' && !visited[i][n-1]){
+                region(board,visited,i,n-1);
+            }
         }
 
+        //converting board 
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(!visited[i][j] && board[i][j]=='O') board[i][j]='X';
+            }
+        }
     }
 }
