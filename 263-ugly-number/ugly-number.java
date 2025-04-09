b448b11010f345aaba411bfd7cac84cda04e53f5
Devansh Kumar <118431081+Devanshkumar007@users.noreply.github.com>
2025-04-10 01:54:42 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (15.52%) - LeetSync

--- 263-ugly-number/ugly-number.java
+++ 263-ugly-number/ugly-number.java
@@ -1,25 +1,11 @@
 class Solution {
     public boolean isUgly(int n) {
         if(n<=0) return false;
-        if(n==1) return true;
-        HashSet<Integer> factors = new HashSet<>();
-        while(n%2==0){
-            factors.add(2);
-            n/=2;
-        }
-        for (int i = 3; i <= Math.sqrt(n); i += 2) {
-            while (n % i == 0) {
-                factors.add(i);
-                n /= i;
-            }
-        }
-        if (n > 2) {
-            factors.add(n);
-        }
 
-        for(int num : factors){
-            if(num!=2 && num!=3 && num!=5) return false;
-        }
-        return true;
+        while(n%2==0) n/=2;
+        while(n%3==0) n/=3;
+        while(n%5==0) n/=5;
+
+        return n==1 ;
     }
 }
