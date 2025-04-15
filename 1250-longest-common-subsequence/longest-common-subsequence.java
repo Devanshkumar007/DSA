513649922d5ea5f24ec6dedef5598112f1d66ad7
Devansh Kumar <118431081+Devanshkumar007@users.noreply.github.com>
2025-04-15 15:01:11 +0530

    Time: 4 ms (99.91%) | Memory: 42.4 MB (93.93%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,12 +1,30 @@
 class Solution {
-    public int longestCommonSubsequence(String text1, String text2) {
-        int dp[][] = new int[text1.length()+1][text2.length()+1];
-        for(int i=1 ; i<text1.length()+1 ; i++){
-            for(int j=1 ; j<text2.length()+1 ; j++){
-                if(text1.charAt(i-1)==text2.charAt(j-1)) dp[i][j]=dp[i-1][j-1]+1;
-                else dp[i][j]=Math.max(dp[i-1][j] , dp[i][j-1]);
+    public int longestCommonSubsequence(String text1, String text2){
+        try {
+            Callable<Integer> func = ()-> solve(text1, text2);
+            FutureTask<Integer> task = new FutureTask(func);
+            new Thread(task).start();
+            return task.get();
+        } catch(Exception ex) {return 0;}
+    }
+    int solve(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+        if(m == 0 || n == 0)
+            return 0;
+        
+        int[] current = new int[n+1];
+        int[] prev = new int[n+1];
+        for(int i=1; i <= m ; i++) {
+            for(int j=1; j <=n; j++) {
+                if(text1.charAt(i-1) == text2.charAt(j-1)) {
+                    current[j] = 1+prev[j-1];
+                } else {
+                    current[j] = Math.max(current[j-1], prev[j]);
+                }
             }
+            System.arraycopy(current, 0, prev, 0, current.length);
         }
-        return dp[text1.length()][text2.length()];
+        return prev[n];
     }
 }
