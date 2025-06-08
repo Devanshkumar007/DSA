1feebb4254f4d082d203e8a722cf29f9eed7a1ea
Devansh Kumar <118431081+Devanshkumar007@users.noreply.github.com>
2025-06-08 19:58:30 +0530

    Time: 2 ms (76.74%) | Memory: 48.8 MB (26.69%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.java
+++ 386-lexicographical-numbers/lexicographical-numbers.java
@@ -3,6 +3,7 @@ class Solution {
         if(curr > n) return;
         ans.add(curr);
         for(int i=0 ; i<10; i++){
+            if( ( (curr*10) + i) > n ) return ;
             dfs(ans, (curr*10) + i , n );
         }
     }
