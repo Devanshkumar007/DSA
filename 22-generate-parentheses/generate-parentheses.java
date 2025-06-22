d2dc93e80627822b5b0751cb5dc1da667b414fbd
Devansh Kumar <118431081+Devanshkumar007@users.noreply.github.com>
2025-06-22 18:05:58 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (94.19%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,28 +1,25 @@
 class Solution {
-    List<String> ans = new ArrayList<>();
-    
-    public void solve(int n, int open, int closed, StringBuilder sb) {
-        if (open == n && closed == n) {
+    public void helper(int n, int open, int close, StringBuilder sb , List<String> ans){
+        if(open==n && close==open){
             ans.add(sb.toString());
-            return;
+            return ;
         }
-
-        if (open > closed) {
-            sb.append(")");
-            solve(n, open, closed + 1, sb);
-            sb.deleteCharAt(sb.length() - 1); 
-        }
-
-        if (open < n) {
+        if(open<n){
             sb.append("(");
-            solve(n, open + 1, closed, sb);
-            sb.deleteCharAt(sb.length() - 1);
+            helper(n,open+1,close,sb,ans);
+            sb.deleteCharAt(sb.length()-1);
+        }
+        if(close < open){
+            sb.append(")");
+            helper(n,open,close+1,sb,ans);
+            sb.deleteCharAt(sb.length()-1);
         }
     }
 
-    public List<String> generateParenthesis(int n) {    
-        StringBuilder sb = new StringBuilder();
-        solve(n, 0, 0, sb);
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList<>();
+        StringBuilder sb= new StringBuilder();
+        helper(n,0,0,sb,ans);
         return ans;
     }
 }
