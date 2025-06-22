7fb330cef43ac09de0c7f35af4853ee80cc3a98a
Devansh Kumar <118431081+Devanshkumar007@users.noreply.github.com>
2025-06-22 17:16:35 +0530

    Time: 20 ms (48.29%) | Memory: 45.4 MB (33.93%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,25 +1,25 @@
 class Solution {
-    public String check(int l , int r , char curr, String s){
-        StringBuilder sb = new StringBuilder();
-        if(curr!='.') sb.append(curr);
+    public String check(int l , int r , String s){
         while(l>=0 && r<s.length() && s.charAt(l)==s.charAt(r)){
-            sb.insert(0,s.charAt(l--)); sb.append(s.charAt(r++));
+            l-=1; r+=1;
         }
-        return sb.toString();
+        return s.substring(l+1,r);
     }
     public String longestPalindrome(String s) {
         String ans = "" ;
         for(int i=0 ; i<s.length() ;i++){
             //odd
-            int l=i-1 ,r =i+1 ; char curr =s.charAt(i);
-            String odd = check(l,r,curr,s);
+            int l=i-1 ,r =i+1 ;
+            String odd = check(l,r,s);
 
             //even 
-            l=i; curr='.';
-            String even = check(l,r,curr,s);
+            l=i; 
+            String even = check(l,r,s);
 
-            String longest = (even.length()>odd.length()) ? new String(even) : new String(odd);
-            if(ans.length()<longest.length()) ans = new String(longest);
+            if(odd.length() > even.length() && odd.length() > ans.length()) ans = new String(odd);
+    else if(odd.length() < even.length() && even.length() > ans.length()) ans = new String(even);
+            // String curr = (even.length()>odd.length()) ? new String(even) : new String(odd);
+            // if(ans.length()<curr.length()) ans = new String(curr);
         }
 
         return ans;
