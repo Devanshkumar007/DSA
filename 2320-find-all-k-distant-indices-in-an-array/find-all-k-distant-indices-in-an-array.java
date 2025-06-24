3b34814eca372c5801aa6cff5075b91bc8bc6d03
Devansh Kumar <118431081+Devanshkumar007@users.noreply.github.com>
2025-06-24 11:55:02 +0530

    Time: 9 ms (57.85%) | Memory: 44.9 MB (60.88%) - LeetSync

--- 2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.java
+++ 2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.java
@@ -1,11 +1,15 @@
 class Solution {
     public List<Integer> findKDistantIndices(int[] nums, int key, int k) {
-        Set<Integer> ans = new TreeSet<>();
+        List<Integer> ans = new ArrayList<>();
+        boolean[] visited = new boolean[nums.length];
         for(int i=0 ;i<nums.length ; i++){
             if(nums[i]==key){
                 int s = (i-k) > 0 ? i-k : 0 ;
                 int e = (i+k) <nums.length ? i+k : nums.length-1;
-                while(s!=e+1) ans.add(s++);
+                while(s!=e+1) { 
+                    if(!visited[s]){ ans.add(s); visited[s]=true ; }
+                    s++; 
+                }
             }
         }
         
