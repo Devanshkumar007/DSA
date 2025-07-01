5a310556b60faf16583bbf1dcd90fdef7d5e6ff3
Devansh Kumar <118431081+Devanshkumar007@users.noreply.github.com>
2025-07-01 09:52:12 +0530

    Time: 1 ms (87.19%) | Memory: 42.4 MB (16.53%) - LeetSync

--- 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.java
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.java
@@ -1,5 +1,9 @@
 class Solution {
-    public int possibleStringCount(String word) {
+    static{
+        for(int i=0 ; i<100 ; i++)
+        possibleStringCount("a");
+    }
+    public static int possibleStringCount(String word) {
         int count = 1 ;
         for(int i=0 ; i<word.length()-1 ; i++){
             if(word.charAt(i)==word.charAt(i+1)) count++;
