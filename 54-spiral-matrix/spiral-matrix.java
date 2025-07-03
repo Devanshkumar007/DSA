53d413f612206034b7e5c7264da60a3edac1e85d
Devansh Kumar <118431081+Devanshkumar007@users.noreply.github.com>
2025-07-03 14:36:28 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (43.21%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.java
+++ 54-spiral-matrix/spiral-matrix.java
@@ -1,37 +1,36 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        int n = matrix.length;
-        int m = matrix[0].length;
+        int n = matrix.length ;
+        int m = matrix[0].length ;
         int top = 0 , bottom = n-1;
-        int left = 0, right = m-1;
-        List<Integer> ans = new LinkedList<>();
-        if(n==0) return ans ;
-
-        while(top <= bottom && left <= right){
-        //right
-        for(int i= left ; i<=right ; i++){
-            ans.add(matrix[top][i]);
+        int left = 0 , right = m-1;
+        List<Integer> ans = new ArrayList<>();
+        while(top<=bottom && left<=right){
+            //left 
+            for(int i=left ; i<=right ; i++){
+                ans.add(matrix[top][i]);
+            }
+            top++;
+            //bottom
+            for(int i=top ; i<=bottom ; i++){
+                ans.add(matrix[i][right]);
+            }
+            right--;
+            //right
+            if(top<=bottom){
+            for(int i=right; i>= left ; i--){
+                ans.add(matrix[bottom][i]);
+            }
+            }
+            bottom--;
+            if(left<=right){
+            //up if
+            for(int i=bottom ; i>=top ; i--){
+                ans.add(matrix[i][left]);
+            }
+            }
+            left++;
         }
-        top++;
-        
-        //bottom
-        for(int i=top; i<=bottom ;i++){
-            ans.add(matrix[i][right]);
-        }
-        right--;
-        if(top <= bottom) {
-        //left
-        for(int i=right ; i>=left ;i--){
-            ans.add(matrix[bottom][i]);
-        }}
-        bottom--;
-        //up
-        if(left <=right){
-        for(int i=bottom ; i>=top ; i--){
-            ans.add(matrix[i][left]);  
-        }}
-        left++;     
-        }
-        return ans ;
+        return ans;
     }
 }
