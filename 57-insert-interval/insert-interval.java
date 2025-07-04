dd2a2edb22efdf14ada60847ba413954fd6d21ef
Devansh Kumar <118431081+Devanshkumar007@users.noreply.github.com>
2025-07-04 14:24:55 +0530

    Time: 47 ms (5.42%) | Memory: 45.8 MB (8.21%) - LeetSync

---
+++ 57-insert-interval/insert-interval.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        ArrayList<int[]> ans = new ArrayList<>();
+        if(intervals.length==0) ans.add(newInterval); 
+        for(int i=0 ; i<intervals.length;i++){
+            if(intervals[i][0] > newInterval[0]) ans.add(newInterval);
+            ans.add(intervals[i]);
+        }
+        if(intervals.length==ans.size()) ans.add(newInterval); 
+        for(int j=0 ; j<ans.size() ; j++){
+            System.out.println(ans.get(j)[0]+" "+ans.get(j)[1]);
+        }
+        int i= 1 ;
+        List<int[]> out = new ArrayList<>();
+        int start = ans.get(0)[0];
+        int end = ans.get(0)[1];
+        while(i<ans.size()){
+            if(ans.get(i)[0] <= end ) end=Math.max(end,ans.get(i)[1]);
+            else{ out.add(new int[]{start,end});
+            start = ans.get(i)[0];
+            end = ans.get(i)[1];
+            }
+            i++;
+        }
+        out.add(new int[]{start,end});
+        int[][] output = new int[out.size()][2];
+
+        for(int j=0 ; j<out.size() ; j++){
+            output[j] = out.get(j);
+        }
+        return output ;
+        
+
+    }
+}
