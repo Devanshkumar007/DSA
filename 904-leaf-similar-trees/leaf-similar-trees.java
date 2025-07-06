4c6d9b9309f79bd6f134753613c935778e9257a7
Devansh Kumar <118431081+Devanshkumar007@users.noreply.github.com>
2025-07-06 19:51:40 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (91.40%) - LeetSync

--- 904-leaf-similar-trees/leaf-similar-trees.java
+++ 904-leaf-similar-trees/leaf-similar-trees.java
@@ -18,8 +18,9 @@ class Solution {
     public void traverse(TreeNode root, Boolean flag){
         if(root==null || !fflag ) return; 
         if(flag && root.left==root.right) hs.add(root.val);
-        if(!flag && root.left==root.right) {if( hs.size() >0 && hs.get(0)==root.val ) hs.remove(0);
-        else fflag = false ;
+        if(!flag && root.left==root.right) {
+            if( hs.size() >0 && hs.get(0)==root.val ) hs.remove(0);
+            else fflag = false ;
         }
 
         traverse(root.left , flag);
