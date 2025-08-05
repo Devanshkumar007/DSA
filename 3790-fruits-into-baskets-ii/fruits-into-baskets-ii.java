3ebe7200dd2cf01ec024f464fcfcc6da0b3d8f5a
Devansh Kumar <118431081+Devanshkumar007@users.noreply.github.com>
2025-08-06 01:27:57 +0530

    Time: 1 ms (100.00%) | Memory: 44.4 MB (60.79%) - LeetSync

--- 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.java
+++ 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.java
@@ -1,12 +1,10 @@
 class Solution {
     public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
-        boolean[] check = new boolean[baskets.length];
         int ans = 0 ;
         for(int i=0;i<fruits.length;i++){
-            int max = 0 ;
             boolean curr = false ;
             for(int j=0;j<baskets.length ; j++){
-                if(!check[j] && fruits[i]<=baskets[j]){ curr = true ; check[j]=true; break;}
+                if(fruits[i]<=baskets[j]){ curr = true ; baskets[j]=0; break;}
             }
             if(!curr) ans++;
         }
