8dde1ee7e43c5c77199cf103671ed48842b0f1d2
Devansh Kumar <118431081+Devanshkumar007@users.noreply.github.com>
2025-08-06 02:34:03 +0530

    Time: 32 ms (86.41%) | Memory: 55.5 MB (32.38%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.java
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -1,33 +1,17 @@
 class Solution {
-    static {
-        for(int i=0; i<1000 ; i++){
-            totalFruit(new int[]{});
-        }
-    }
-    public static int totalFruit(int[] fruits) {
-        HashMap<Integer,Integer> mp = new HashMap<>();
-        int[] unique = new int[2];
+    public int totalFruit(int[] fruits) {
+        LinkedHashMap<Integer,Integer> mp = new LinkedHashMap<>(2);
         int left = 0 ;
         int max = 0 ;
-
         for(int i=0; i<fruits.length ; i++){
-            if(mp.size()==0){
-                unique[0] = fruits[i]; 
-            }else if(mp.size()==1){
-                unique[1]= fruits[i];
-            }else{
+           if (mp.size()==2){
                 if(!mp.containsKey(fruits[i])){
-                    int prev = fruits[i-1];
                     max = Math.max(i-left, max);  
-                    if(prev == unique[0]){
-                        left = mp.get(unique[1]) + 1;
-                        mp.remove(unique[1]);
-                        unique[1]= fruits[i];
-                    }else{
-                        left = mp.get(unique[0]) + 1;
-                        mp.remove(unique[0]);
-                        unique[0]= fruits[i];
-                    }
+                    Iterator<Integer> it = mp.keySet().iterator();
+                    int key = it.next();
+                    if(fruits[i-1] == key) key = it.next();
+                    left = mp.get(key)+1;
+                    mp.remove(key);
                 }
             }
                 mp.put(fruits[i],i);
