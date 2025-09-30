2520897c6d1648fd399701cd581728aaa966c67c
Devansh Kumar <118431081+Devanshkumar007@users.noreply.github.com>
2025-09-30 15:53:59 +0530

    Time: 42 ms (86.32%) | Memory: 44.5 MB (78.77%) - LeetSync

---
+++ 2324-find-triangular-sum-of-an-array/find-triangular-sum-of-an-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int triangularSum(int[] nums) {
+        int n = nums.length;
+        while(n!=1){
+            for(int j=0 ; j<n-1 ; j++){
+                nums[j] = (nums[j]+nums[j+1])%10;
+            }
+            n--;
+        }
+        return nums[0];
+    }
+}
