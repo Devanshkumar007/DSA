886904d670699a2ec32157c8fe0753924c794150
Devansh Kumar <118431081+Devanshkumar007@users.noreply.github.com>
2025-10-05 02:31:06 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (18.53%) - LeetSync

--- 1642-water-bottles/water-bottles.java
+++ 1642-water-bottles/water-bottles.java
@@ -1,15 +1,9 @@
 class Solution {
     public int numWaterBottles(int numBottles, int numExchange) {
         int ans = numBottles ;
-        int empty= numBottles ;
-        int left = 0 ;
-        while(empty>=numExchange){
-            //water that can be purchased from empty
-            ans += empty/numExchange;
-            // empty bottles left unused for puchase
-            left = empty % numExchange ;
-            //new empty bottles acquired
-            empty = (empty / numExchange) + left ;
+        while(numBottles >=numExchange){
+            ans += numBottles/numExchange;
+            numBottles = (numBottles / numExchange) + (numBottles % numExchange);
         }
         return ans ;
     }
