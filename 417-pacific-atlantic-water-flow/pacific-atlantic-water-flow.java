729750fb7efd58b151e7c9a477bbea54283b6047
Devansh Kumar <118431081+Devanshkumar007@users.noreply.github.com>
2025-10-05 23:04:53 +0530

    Time: 9 ms (37.31%) | Memory: 46 MB (21.58%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
@@ -1,62 +1,57 @@
-class Pair{
-    int i;
-    int j;
-    Pair(int i ,int j){
-        this.i=i;
-        this.j=j;
-    }
-}
-
 class Solution {
+    int[][] dirs = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        int m = heights.length, n = heights[0].length;
+
+        boolean[][] pacific = new boolean[m][n];
+        boolean[][] atlantic = new boolean[m][n];
+        Queue<int[]> pq = new LinkedList<>();
+        Queue<int[]> aq = new LinkedList<>();
+
+        // Add border cells
+        for (int i = 0; i < m; i++) {
+            pq.offer(new int[]{i, 0});
+            aq.offer(new int[]{i, n - 1});
+            pacific[i][0] = true;
+            atlantic[i][n - 1] = true;
+        }
+        for (int j = 0; j < n; j++) {
+            pq.offer(new int[]{0, j});
+            aq.offer(new int[]{m - 1, j});
+            pacific[0][j] = true;
+            atlantic[m - 1][j] = true;
+        }
+
+        bfs(heights, pq, pacific);
+        bfs(heights, aq, atlantic);
+
+        // Collect intersection
         List<List<Integer>> ans = new ArrayList<>();
-        //boolean[][] visited = new boolean[heights.length][heights[0].length];
-        // pacific i<0 && j<0
-        // atlantic i>=n && j>=n 
-        // System.out.println(heights.length + " "+heights[0].length);
-
-        for(int i=0 ; i<heights.length ; i++){
-            for(int j=0 ; j<heights[i].length ; j++){
-                    if(check(heights , i , j)) ans.add(new ArrayList<>(List.of(i,j)));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (pacific[i][j] && atlantic[i][j]) {
+                    ans.add(List.of(i, j));
+                }
             }
         }
-            return ans; 
-
+        return ans;
     }
 
-    public boolean check(int[][] heights , int i,  int j ){
-        boolean pacific = false ;
-        boolean atlantic = false ;
-        int m = heights.length;
-        int n = heights[0].length;
-        boolean[][] visited = new boolean[m][n];
-        visited[i][j] = true ;
-        // System.out.println("Current Node : "+i+" "+j);
-        
-        int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(i,j));
-
-        while(!q.isEmpty()){
-            int row = q.peek().i;
-            int col = q.peek().j;
-            q.remove();
-
-            for(int k=0 ;k<4; k++){
-                int r = row + dir[k][0];
-                int c = col + dir[k][1];
-                if(r>=0 && r<m && c>=0 && c<n && heights[r][c]<=heights[row][col] && !visited[r][c]){
-                    // System.out.println("In queue: "+r+" "+c);
-                    visited[r][c] = true ;
-                    q.add(new Pair(r,c));
-                }else if(r<0 || c<0) pacific = true ;
-                else if(r==m || c==n ) atlantic = true;
-
-                if(pacific && atlantic) return true ;
+    private void bfs(int[][] heights, Queue<int[]> q, boolean[][] ocean) {
+        int m = heights.length, n = heights[0].length;
+        while (!q.isEmpty()) {
+            int[] cell = q.poll();
+            int i = cell[0], j = cell[1];
+            for (int[] d : dirs) {
+                int r = i + d[0], c = j + d[1];
+                if (r >= 0 && r < m && c >= 0 && c < n &&
+                    !ocean[r][c] &&
+                    heights[r][c] >= heights[i][j]) {
+                    ocean[r][c] = true;
+                    q.offer(new int[]{r, c});
+                }
             }
         }
-
-        return false ;
     }
-
 }
