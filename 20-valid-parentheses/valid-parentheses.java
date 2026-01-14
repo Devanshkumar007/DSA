ee0cc8cfa0819d43a52d756a6afc497ee298312d
Devansh Kumar <118431081+Devanshkumar007@users.noreply.github.com>
2026-01-14 22:23:24 +0530

    Time: 3 ms (87.58%) | Memory: 43.6 MB (19.28%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -4,10 +4,10 @@ class Solution {
 		for(int i=0; i<s.length(); i++) {
 			char curr= s.charAt(i);
 			if(curr=='(' || curr=='{' || curr=='[') st.push(curr);
-			else if(!st.isEmpty() && curr==')' && st.peek()!='(' ) return false; 
-			else if(!st.isEmpty() && curr=='}' && st.peek()!='{') return false; 
-			else if(!st.isEmpty() && curr==']' && st.peek()!='[') return false;
 			else if(st.isEmpty()) return false;
+			else if(curr==')' && st.peek()!='(' ) return false; 
+			else if(curr=='}' && st.peek()!='{') return false; 
+			else if(curr==']' && st.peek()!='[') return false;
             else st.pop();
 		}
 		return st.isEmpty();
